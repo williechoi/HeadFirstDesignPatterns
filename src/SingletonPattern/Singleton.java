d20065090615a8a32ef1dda61c2fdae6be2fcaf3
Hyungsuk Choi <choihyungsuk@gmail.com>
2023-03-21 18:57:48 +0900

    singleton pattern implemented

---
+++ src/SingletonPattern/Singleton.java
@@ -0,0 +1,13 @@
+package SingletonPattern;
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+    private Singleton() {}
+
+    public static synchronized Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
