82d19d7c6be1a9de8d5abe20adf340dc0883a660
Hyungsuk Choi <choihyungsuk@gmail.com>
2023-03-26 23:33:20 +0900

    implement another iterator with map-based object

---
+++ src/IteratorPattern/CafeMenu.java
@@ -0,0 +1,35 @@
+package IteratorPattern;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class CafeMenu implements Menu {
+    Map<String, MenuItem> menuItems = new HashMap<String, MenuItem>();
+
+    public CafeMenu() {
+        addItem("Veggie Burger and Air Fries",
+                "Veggie Burger on a whole wheat bun, lettuce, tomato, and fries",
+                true, 3.99);
+        addItem("Soup of the day",
+                "A cup of the soup of the day, with a side salad",
+                false, 3.69);
+        addItem("Burrito",
+                "A large burrito, with whole pinto beans, salsa, and guacamole",
+                true, 4.29);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        menuItems.put(name, menuItem);
+    }
+
+    public Map<String, MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    @Override
+    public Iterator<MenuItem> createIterator() {
+        return menuItems.values().iterator();
+    }
+}
